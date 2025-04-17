6b1a1dd38a1854f3515394733c25a6d35db05809
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2025-04-17 18:03:53 +0500

    Upper case converter

---
+++ UpperCase.py
@@ -0,0 +1,8 @@
+# Ask the user for the food they would like
+food = input("What kind of food would you like? ")
+
+# Convert the food name to uppercase
+food_upper = food.upper()
+
+# Print the message with the food name in uppercase
+print(f"Let me see if I can find you a {food_upper}.")
