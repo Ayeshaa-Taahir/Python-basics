3ea0f506e5d344aa7fe84570c66c05d42d596e1a
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2025-04-17 19:25:00 +0500

    Operations in python

---
+++ Operations.py
@@ -0,0 +1,11 @@
+# Performing the operations
+add_result = 50 + 4
+sub_result = 50 - 4
+mul_result = 50 * 4
+div_result = 50 / 4 
+
+# Displaying the results
+print("i. 50 + 4 =", add_result)
+print("ii. 50 - 4 =", sub_result)
+print("iii. 50 * 4 =", mul_result)
+print("iv. 50 / 4 =", div_result)
