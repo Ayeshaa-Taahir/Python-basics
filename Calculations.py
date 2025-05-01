20c1478667a2bf0641d537215b3fbc0903c5b568
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2025-05-01 23:44:01 +0500

    Expression execution

--- Calculations.py
+++ Calculations.py
@@ -2,3 +2,13 @@ a=4
 b=5
 sum =a+b
 print(sum)
+
+# Expression execution
+
+Txt="shimlaaaa" #string and numeric value can operate together
+print(5*Txt*3)
+
+#string and string can operate with +
+A="5"
+Txt="@"
+print((A+Txt)*3) #the term used for adding two string is called Concatenation
